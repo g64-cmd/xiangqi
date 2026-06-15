1939439923775dc1ddd7fc4dea4fc1370ab30c32
g64-cmd <23251019@bjtu.edu.cn>
2026-06-16 02:16:29 +0800

    feat(M4): 新增 SetupViewModel + SetupUiState
    - SetupUiState: 默认人机 / 红方 / 中级
    - SetupViewModel: onModeChange/onSideChange/onDifficultyChange 同步 update
    - onStart: 写入 GameConfigHolder,orientation 按模式派生(HOT_SEAT=RED,
      HUMAN_VS_AI=humanSide),然后触发导航回调

---
+++ app/src/main/java/com/xiangqi/app/ui/setup/SetupUiState.kt
@@ -0,0 +1,21 @@
+package com.xiangqi.app.ui.setup
+
+import com.xiangqi.app.domain.model.Side
+import com.xiangqi.app.engine.Difficulty
+import com.xiangqi.app.ui.game.GameMode
+
+/**
+ * SetupScreen 的 UI 状态。
+ *
+ * 由 [SetupViewModel] 持有,SetupScreen 通过 `collectAsStateWithLifecycle` 订阅。
+ * 玩家每次修改选项后通过 `update` 推送新值。
+ *
+ * @property mode 对局模式,默认人机。
+ * @property humanSide 人机模式下玩家执棋方,默认红方。
+ * @property difficulty 人机模式下 AI 难度档位,默认中级。
+ */
+data class SetupUiState(
+    val mode: GameMode = GameMode.HUMAN_VS_AI,
+    val humanSide: Side = Side.RED,
+    val difficulty: Difficulty = Difficulty.INTERMEDIATE,
+)
